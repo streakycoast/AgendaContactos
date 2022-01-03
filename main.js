cfe3ff6f3d422ce18561b722f1a86ad9bda03661
Francisco Javier Carranza Esquivel <60407692+Carranza12@users.noreply.github.com>
2022-01-03 15:27:32 -0600

    ya funciona local storage

--- main.js
+++ main.js
@@ -1,12 +1,55 @@
-let nombre= document.getElementById('nombre').value 
-let apellido= document.getElementById('apellido').value 
-let telefono= document.getElementById('phone').value 
-let correo= document.getElementById('email').value 
-
 const botonAdd = document.getElementById('btn-add')
 
-botonAdd.addEventListener('click', (e)=>{
-    e.preventDefault()
-    console.log(nombre )
-})
+   
+
+
+
+const guardarForm =(e)=>{
+    e.preventDefault(); //evita que la pagina se refresque cuando das click
+  
+    var nombre=document.getElementById('nombre').value
+    var apellido=document.getElementById('apellido').value
+    var telefono=document.getElementById('phone').value
+    var correo=document.getElementById('email').value
+
+    const datosContacto={
+        "nombre":nombre,
+        "apellido":apellido,
+        "telefono": telefono,
+        "correo":correo
+      }
+   
+    
+
+    let idAleatorio= Math.random(); // genera un id unico
+  
+    let contactoString=localStorage.setItem(idAleatorio, JSON.stringify(datosContacto)); //agregar el objeto al localStorage
+
+   let stringAJson = localStorage.getItem(idAleatorio); //obtener el elemento del localStorage
+   
+    let contactos= (idAleatorio, JSON.parse(stringAJson)); //convertir ese elemento a json
+    
+    console.log(contactos)
+
+    
+      function mostrarDatos(){
+          console.log("mostrando datos" ,contactos)
+      }
+
+      
+          mostrarDatos();
+      
+
+    
+
+
+}
+
+
+
+const botonMostrar=document.getElementById('btn-mostrar');
+    
+botonAdd.addEventListener('click', guardarForm)
+
+
 
